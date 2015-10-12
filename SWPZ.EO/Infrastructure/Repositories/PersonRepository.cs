8a96d13cf26ac5b56b60bfb9c1ae3dfdcbb1c6a8
alisious <alisious@wp.pl>
2015-10-12 15:24:20 +0200

    Dodanie PersonRepository

---
+++ SWPZ.EO/Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWPZ.EO.Domain;
+
+namespace SWPZ.EO.Infrastructure.Repositories
+{
+    public class PersonRepository :IPersonRepository
+    {
+        public Person Load(int personId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
